e781d6764d0ae8bf2a4d29c1ae69777ec836a16d
Jason Maderski <maderski@gmail.com>
2025-04-10 18:12:30 -0400

    create screen and hook up viewModel

--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/MainActivity.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.maderskitech.androidcodingexerciseapp.ui.theme.AndroidCodingExerciseAppTheme
+import com.maderskitech.kmpcodingexercisenetwork.domain.model.Item
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -24,30 +31,73 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidCodingExerciseAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    DisplayItemsScreen(
+                        isLoading = viewModel.isLoading,
+                        itemList = viewModel.items,
+                        innerPadding = innerPadding
                     )
                 }
             }
         }
 
-        viewModel.printSortedItemsToTerminal()
+        viewModel.fetchItems()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DisplayItemsScreen(
+    isLoading: Boolean,
+    itemList: List<Item>,
+    innerPadding: PaddingValues
+) {
+    if (isLoading) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = innerPadding
+        ) {
+            items(
+                items = itemList,
+                key = { it.id }
+            ) { item ->
+                   ListItem(
+                       headlineContent = {
+                           Text(item.name)
+                       },
+                       supportingContent = {
+                           Text("ID: ${item.id}")
+                       },
+                       trailingContent = {
+                           Text("List ID: ${item.listId}")
+                       }
+                   )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewDisplayItemsScreen() {
+    DisplayItemsScreen(
+        false,
+        listOf(Item(100, 1, "Item 100"), Item(101, 2, "Item 101"), Item(202, 3, "Item 202")),
+        PaddingValues(8.dp)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AndroidCodingExerciseAppTheme {
-        Greeting("Android")
-    }
+fun PreviewDisplayItemsScreenLoading() {
+    DisplayItemsScreen(
+        true,
+        emptyList(),
+        PaddingValues(8.dp)
+    )
 }
--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/MainViewModel.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/MainViewModel.kt
@@ -1,23 +1,32 @@
 package com.maderskitech.androidcodingexerciseapp
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.maderskitech.kmpcodingexercisenetwork.domain.model.Item
 import com.maderskitech.kmpcodingexercisenetwork.domain.usecase.SortedItemsUseCase
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val sortedItemsUseCase: SortedItemsUseCase) : ViewModel() {
-    fun printSortedItemsToTerminal() {
+    var isLoading by mutableStateOf(false)
+        private set
+
+    var items by mutableStateOf<List<Item>>(emptyList())
+        private set
+    
+    fun fetchItems() {
+        isLoading = true
         viewModelScope.launch {
+            isLoading = true
             sortedItemsUseCase.getSortedItemsFlow().collect { result ->
                 result
                     .onSuccess { listIdToItemsMap ->
-                        listIdToItemsMap.forEach { entry ->
-                            Log.d(TAG, "${entry.key}")
-                            entry.value.forEach { item ->
-                                Log.d(TAG, item.name)
-                            }
-                        }
+                        items = listIdToItemsMap.values.flatten()
+                        isLoading = false
                     }
                     .onFailure { throwable ->
                         Log.e(TAG, "Error Occurred getting sorted items.\nMessage: ${throwable.message}")
