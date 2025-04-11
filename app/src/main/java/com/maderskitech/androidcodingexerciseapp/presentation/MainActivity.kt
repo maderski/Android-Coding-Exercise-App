d6f216dc3c2a86ca53982cadb8ddcdf7ec74c8f7
Jason Maderski <maderski@gmail.com>
2025-04-10 22:31:19 -0400

    add safe drawing padding

--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/presentation/MainActivity.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/presentation/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -21,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +42,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidCodingExerciseAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier
+                    .background(Color.Black)
+                    .safeDrawingPadding()
+                ) { innerPadding ->
                     DisplayItemsScreen(
                         isLoading = viewModel.isLoading,
                         itemGroups = viewModel.itemGroups,
