28ae167026d20446da7b7e1a001b035568f25b6e
Jason Maderski <maderski@gmail.com>
2025-04-15 19:01:35 -0400

    use initKoin from library

--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/application/MainApplication.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/application/MainApplication.kt
@@ -3,6 +3,7 @@ package com.maderskitech.androidcodingexerciseapp.application
 import android.app.Application
 import com.maderskitech.androidcodingexerciseapp.application.di.appModule
 import com.maderskitech.kmpcodingexercisenetwork.di.commonModule
+import com.maderskitech.kmpcodingexercisenetwork.di.initKoin
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -11,10 +12,6 @@ import org.koin.core.context.startKoin
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-          androidContext(this@MainApplication)
-          androidLogger()
-          modules(appModule + commonModule)
-        }
+        initKoin(listOf(appModule))
     }
 }
