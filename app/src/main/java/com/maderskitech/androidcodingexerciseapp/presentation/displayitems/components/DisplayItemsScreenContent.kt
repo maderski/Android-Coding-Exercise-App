a590851bf8a598e48063a4baa31647554001ca53
Jason Maderski <maderski@gmail.com>
2025-04-17 18:00:32 -0400

    change package from composables to components

--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/presentation/displayitems/components/DisplayItemsScreenContent.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/presentation/displayitems/components/DisplayItemsScreenContent.kt
@@ -1,4 +1,4 @@
-package com.maderskitech.androidcodingexerciseapp.presentation.displayitems.composables
+package com.maderskitech.androidcodingexerciseapp.presentation.displayitems.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
