067559e8c4ef7ec81461a7b637d7d9e6d78c756a
Jason Maderski <maderski@users.noreply.github.com>
2025-04-16 22:32:53 -0400

    chore: merge develop into main for v1.0.1 (#3)
    * fix dark/light theme issues with status bar
    
    * use param activity instead of casting the view context as an activity
    
    * bump version
    
    * use initKoin from library
    
    * use updated network aar
    
    * update to use new version of KMP Network library

--- app/src/main/java/com/maderskitech/androidcodingexerciseapp/application/MainApplication.kt
+++ app/src/main/java/com/maderskitech/androidcodingexerciseapp/application/MainApplication.kt
@@ -2,19 +2,11 @@ package com.maderskitech.androidcodingexerciseapp.application
 
 import android.app.Application
 import com.maderskitech.androidcodingexerciseapp.application.di.appModule
-import com.maderskitech.kmpcodingexercisenetwork.di.commonModule
-
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
+import com.maderskitech.kmpcodingexercisenetwork.di.KoinHelper
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-          androidContext(this@MainApplication)
-          androidLogger()
-          modules(appModule + commonModule)
-        }
+        KoinHelper.initKoin(listOf(appModule))
     }
 }
